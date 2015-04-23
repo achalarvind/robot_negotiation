6836e85560eb7df41f45df517545569a9bc2d6f2
kobotics <baraka.kim@gmail.com>
2015-04-23 14:44:10 -0400

    Changes in CobotQueueHandler

--- src/CobotQueueHandler.cpp
+++ src/CobotQueueHandler.cpp
@@ -1,8 +1,11 @@
+#include "ros/ros.h"
 #include "../include/CobotQueueHandler.h"
+#include <algorithm>
 
-cobotQueue::cobotQueue(std::vector<uint> ids)
+cobotQueue::cobotQueue(std::vector<uint> ids, uint maxN)
 {
 	cobotIds = ids;
+	maxNDecisionMakers = maxN;
 }
 
 uint cobotQueue::get_queue_size()
@@ -35,17 +38,33 @@ bool cobotQueue::initiate_vote() // Broadcasts a message to all CoBots in Queue 
 	return false;
 }
 
-bool cobotQueue::collect_votes() // Listens to incoming messages and collects votes for all Cobots present in Queue (returns matrix of votes by reference)
+std::vector<std::vector<uint*>> cobotQueue::collect_votes(uint nPlans) // Listens to incoming messages and collects votes for all Cobots present in Queue (returns matrix of votes (rows are ranks; cols are voter IDs))
 {
+
 	return false;
 }
 
-bool cobotQueue::send_best_plan() // Sends final plan to Baxter
+bool cobotQueue::send_best_plan(std::vector<std::vector<uint*>> votesTable, uint nPlans) // Evaluates votes and send final plan to Baxter
 {
+	//create an array of scores for every plan 
+
+	////
+
+ 	for(int rank = 0; rank < votesTable.size(); rank++)
+ 	{
+ 		for(int voter = 0; voter < votesTable[rank].size(); voter++)
+ 		{
+ 			if()
+ 		}
+
+ 	} 
+	
+
 	return false;
 }
 
 int main(){
 
+    send_best_plan
 	return 0;
 }
