b39fd95d9525247cbf56c2c88d88919f3bccd226
kobotics <kim@kim-HP-Pavilion-dv6-Notebook-PC.(none)>
2015-04-25 23:00:17 -0400

    Voting updated

--- include/CobotQueueHandler.h
+++ include/CobotQueueHandler.h
@@ -2,10 +2,10 @@
 
 class cobotQueue{	
 	private:
-		ros::NodeHandle nh;
+		//ros::NodeHandle nh;
 		// ros::Publisher cobotQueuePub; //publishes number of Cobots in queue, among possibly other things
 		std::vector<uint> cobotIds; //a list of the ID's of Cobot in queue
-		uint maxNDecisionMakers = 10; //maximum number of Cobots which can participate in plan decision-making
+		uint maxNDecisionMakers; //maximum number of Cobots which can participate in plan decision-making
 
 	public:
 
@@ -25,9 +25,9 @@ class cobotQueue{
 
 		bool initiate_vote();
 
-		bool collect_votes();
+		std::vector< std::vector<uint> > collect_votes(uint nPlans);
 
-		bool send_best_plan();
+		bool send_best_plan(std::vector< std::vector<uint> > votesTable, uint nPlans);
 
 };	
 	
--- src/CobotQueueHandler.cpp
+++ src/CobotQueueHandler.cpp
@@ -2,6 +2,7 @@
 #include "../include/CobotQueueHandler.h"
 #include <algorithm>
 
+
 cobotQueue::cobotQueue(std::vector<uint> ids, uint maxN)
 {
 	cobotIds = ids;
@@ -38,33 +39,39 @@ bool cobotQueue::initiate_vote() // Broadcasts a message to all CoBots in Queue 
 	return false;
 }
 
-std::vector<std::vector<uint*>> cobotQueue::collect_votes(uint nPlans) // Listens to incoming messages and collects votes for all Cobots present in Queue (returns matrix of votes (rows are ranks; cols are voter IDs))
+std::vector< std::vector<uint> > cobotQueue::collect_votes(uint nPlans) // Listens to incoming messages and collects votes for all Cobots present in Queue (returns matrix of votes (rows are ranks; cols are voter IDs))
 {
-
-	return false;
+    std::vector< std::vector<uint> > votesTable;
+	return votesTable;
 }
 
-bool cobotQueue::send_best_plan(std::vector<std::vector<uint*>> votesTable, uint nPlans) // Evaluates votes and send final plan to Baxter
+bool cobotQueue::send_best_plan(std::vector< std::vector<uint> > votesTable, uint nPlans) // Evaluates votes and send final plan to Baxter
 {
 	//create an array of scores for every plan 
 
 	////
-
- 	for(int rank = 0; rank < votesTable.size(); rank++)
+	double* row_sum;
+	row_sum = new double[nPlans];
+ 	for(int plan = 0; plan < votesTable.size(); plan++)
  	{
- 		for(int voter = 0; voter < votesTable[rank].size(); voter++)
+		row_sum[plan] = 0;
+ 		for(int voter = 0; voter < votesTable[plan].size(); voter++)
  		{
- 			if()
+ 			row_sum[plan] = row_sum[plan] + votesTable[plan][voter];
  		}
-
+		row_sum[plan] = row_sum[plan]/votesTable[plan].size();
+		std::cout<<(boost::lexical_cast<std::string>(row_sum[plan]))<<std::endl;
  	} 
 	
-
 	return false;
 }
 
 int main(){
-
-    send_best_plan
+ 	std::vector< std::vector<uint> > votesTable {{10,9,8,7,6},{10,9,8,7,6},{1,2,3,4}};
+ 	//votesTable[0] = {10,9,8,7,6};
+	//votesTable[1] = {9,10,7,8,6};
+	//votesTable[2] = {1,2,3,4};  
+    cobotQueue c({},3);
+    c.send_best_plan(votesTable,3);
 	return 0;
 }
