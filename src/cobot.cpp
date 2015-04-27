70a3a1d370458cdcbb4700e4ecc61492246e49a9
kobotics <baraka.kim@gmail.com>
2015-04-27 17:21:11 -0400

    Finalized voting in cobot class

--- src/cobot.cpp
+++ src/cobot.cpp
@@ -31,26 +31,7 @@ ccobot::ccobot(uint cobotId){
  	return costs;
  }
 
-double ccobot::plan_cost(std::vector<DeliveryOrderSeq> plan)
-{
-
-	std::string s;
-	DeliveryOrderSeq a(1,s,0.0,0.0);
-	for(int i =0; i < plan.size(); i++)
-	{
-		//if(plan[i].m_iCobotNum == id) {a.set(plan[i]);  break;}
-	}
-	//count_missed_deadlines(a.m_dExpectedTime,tasks);
-	// std::string s;
-	// CSPSolver::DeliveryOrderSeq a(1,s,0.0,0.0);
-	// for(int i =0; i < plan.size(); i++)
-	// {
-	// 	if(plan[i].m_iCobotNum == id) {a = plan[i];  break;}
-	// }
-        return 0;	
-}
-
-int count_missed_deadlines(double start, std::vector<robot_negotiation::Task> tasks)  //given the start time of the schedule, how many deadlines are missed in the best case?
+int count_missed_deadlines(double start, std::vector<robot_negotiation::Task> rem_tasks)  //given the start time of the schedule, how many deadlines are missed in the best case?
 {
 	int count = 0;
 	std::vector<double> deadlines;
@@ -58,17 +39,32 @@ int count_missed_deadlines(double start, std::vector<robot_negotiation::Task> ta
 	double last_end = start;
 	double curr_end = start;
 	//store end times for each task and compare them to deadlines of each task
-	for(int i = 0; i < tasks.size(); i++)
+	for(int i = 0; i < rem_tasks.size(); i++)
 	{
-		curr_end = last_end + tasks[i].est_time;
+		curr_end = last_end + rem_tasks[i].est_time;
 		end_times.push_back(curr_end);
 		std::cout<<curr_end<<",";
-		deadlines.push_back(tasks[i].deadline);
-		std::cout<<tasks[i].deadline<<std::endl;
-		if(curr_end > tasks[i].deadline) count += 1;
+		deadlines.push_back(rem_tasks[i].deadline);
+		std::cout<<rem_tasks[i].deadline<<std::endl;
+		if(curr_end > rem_tasks[i].deadline) count += 1;
 		last_end = curr_end; 
 	}
 	return count;
+}
+
+
+double ccobot::plan_cost(std::vector<DeliveryOrderSeq> plan)
+{
+
+	std::string s;
+	DeliveryOrderSeq a(0,s,0,0);
+	for(int i =0; i < plan.size(); i++)
+	{
+		if(plan[i].m_iCobotNum == id) {a = plan[i];  break;}
+	}
+	int nmissed =  count_missed_deadlines(a.m_dExpectedTime,tasks.task_list);
+    
+    return nmissed;
 }
 
 
@@ -85,6 +81,20 @@ int main(int argc, char **argv){
 
 /*int main()
 {
+    std::vector<DeliveryOrderSeq> plan;
+    DeliveryOrderSeq a1(0,"",1,0);
+    DeliveryOrderSeq a2(2,"",3,3);
+    DeliveryOrderSeq a3(3,"",6,0);
+    DeliveryOrderSeq a4(1,"",10,0);
+
+    plan.push_back(a1);
+    plan.push_back(a2);
+    plan.push_back(a3);
+    plan.push_back(a4);
+
+    ccobot c(2);
+    std::cout<<c.plan_cost(plan)<<std::endl;
+    
 	robot_negotiation::TaskList tasks;
     robot_negotiation::Task t1;
     t1.est_time = 1;
@@ -102,6 +112,6 @@ int main(int argc, char **argv){
     tasks.task_list.push_back(t2);
     tasks.task_list.push_back(t3);
     tasks.task_list.push_back(t4);
-    std::cout<<count_missed_deadlines(1, tasks.task_list)<<std::endl;
+    //std::cout<<count_missed_deadlines(1, tasks.task_list)<<std::endl;
 	return 0;
 }*/
