2905a9d43c1ac325e47c7ce9950b6ffbfef50a1d
jmogali <jmogali@andrew.cmu.edu>
2015-04-28 12:16:48 -0400

    Changes made for testing
    Returns makespan of schedule

--- include/CSPSolver.h
+++ include/CSPSolver.h
@@ -97,21 +97,24 @@ class CSPSolver
 		void CheckForLocalImprovementGreedyStartegy(std::vector<DeliveryOrderSeq>* pvecDeliverySequence);
 
 		void PopulateSequenceInfo();
-		void InsertSequenceIntoCandidatePool(double, std::vector<DeliveryOrderSeq>);
+		void InsertSequenceIntoCandidatePool(double, std::vector<DeliveryOrderSeq>*);
 		double ReturnKeyOfBestCandidate();
 		int Return_MCV_Cobot(std::vector<int>* pvecVals);
 		bool CheckIfAllVarsInitialized();
 		std::tuple<double, int, Location> GetPairWiseShortestCosts(std::vector<Environment>* pvecEnvVars, Block obBlock, int iCurr, int iDest , double dCurrTime);
-	
+		double ReturnTimeToPlaceObject(std::string stCobotType , int iTableNum);
 public:
-		double m_d_WorldStartTime;
+	
+	    double m_d_WorldStartTime;
 	    CSPSolver(std::string strStartLoc, double dStartTime, double dTime_Out_1, std::vector<Environment*> , EnvironmentGeometry obGeometry);
 
-#ifdef ROS_CODE	
+#ifdef ROS_VERSION	
 		std::unordered_map<double, std::vector<DeliveryOrderSeq>> GenerateCobotOrder(std::vector<TaskInfo>);
 #else
 		std::unordered_map<double, std::vector<DeliveryOrderSeq>> GenerateCobotOrder(std::vector<TaskInfo>, bool);
 #endif
+
+		double ReturnMakeSpanForSchedule(std::vector<DeliveryOrderSeq>* pvecDeliverySequence);
 
 		typedef std::pair<std::pair<int , double>, std::pair<double, Location >> PickUpTime;
 		typedef std::pair<int, std::pair<double, int >> DeliveryTime;
--- include/Constants.h
+++ include/Constants.h
@@ -4,9 +4,22 @@
 #include<limits>
 
 const double MAX_DIST_VALUE = std::numeric_limits<double>::infinity();
-const double BAXTER_TRAV_SPEED = 2.0;
-const double BAXTER_PICK_UP_SPEED = 0.05;
-const double TIME_TO_PLACE_OBJECT = 2.0;
+const double BAXTER_TRAV_SPEED = .5;         //IN M/SEC
+const double BAXTER_PICK_UP_SPEED = 0.15;   // IN M/SEC
+const double TIME_TO_PLACE_OBJECT = 5.0;   // IN SECONDS  
+
+const double TIME_TO_PLACE_OBJECT_TABLE_0_SMALL = 5.0;   // IN SECONDS  
+const double TIME_TO_PLACE_OBJECT_TABLE_0_MEDIUM = 5.0;   // IN SECONDS  
+const double TIME_TO_PLACE_OBJECT_TABLE_0_LARGE = 5.0;   // IN SECONDS  
+
+const double TIME_TO_PLACE_OBJECT_TABLE_1_SMALL = 5.0;   // IN SECONDS  
+const double TIME_TO_PLACE_OBJECT_TABLE_1_MEDIUM = 5.0;   // IN SECONDS  
+const double TIME_TO_PLACE_OBJECT_TABLE_1_LARGE = 5.0;   // IN SECONDS  
+
+const double TIME_TO_PLACE_OBJECT_TABLE_2_SMALL = 5.0;   // IN SECONDS  
+const double TIME_TO_PLACE_OBJECT_TABLE_2_MEDIUM = 5.0;   // IN SECONDS  
+const double TIME_TO_PLACE_OBJECT_TABLE_2_LARGE = 5.0;   // IN SECONDS  
+
 const std::string NAMELESS_BLOCK = " ";
 const double MAX_DELAY_TIME_PROP_DISTANCE = 0.25;
 
